5cea1bbc3a16b308002f1713c8fac68f716e4832
kanta14706 <kanta14706@gmail.com>
2026-03-14 11:06:21 +0000

    SRLI done

--- asm.c
+++ asm.c
@@ -219,8 +219,12 @@ int inst_to_binary(
          * Lab2-1 assignment
          * tip: you may need the function `lower5bit`
          */
-        warn("Lab2-1 assignment: SRLI instruction\n");
-        exit(EXIT_FAILURE);
+        binary = (0x05 << 12) + (0x04 << 2) + 0x03; // funct3 + opcode
+		binary += (reg_to_num(arg1, line_no) << 7); // destination reg
+		binary += (reg_to_num(arg2, line_no) << 15); // source reg (rs1)
+		binary += (lower5bit(arg3, line_no) << 20);
+        /* warn("Lab2-1 assignment: SRLI instruction\n");
+        exit(EXIT_FAILURE); */
     } else if (is_opcode(opcode) == SRAI) {
         /*
          * Lab2-1 assignment
