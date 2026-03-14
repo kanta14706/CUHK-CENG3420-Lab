9698c34ee1c9f1055ce0b8da7f5531017c658241
kanta14706 <kanta14706@gmail.com>
2026-03-14 11:25:46 +0000

    ANDI done

--- asm.c
+++ asm.c
@@ -246,8 +246,12 @@ int inst_to_binary(
         exit(EXIT_FAILURE); */
     } else if (is_opcode(opcode) == ANDI) {
         /* Lab2-1 assignment */
-        warn("Lab2-1 assignment: ANDI instruction\n");
-        exit(EXIT_FAILURE);
+        binary = (0x07 << 12) + (0x04 << 2) + 0x03 // funct3 + opcode
+        binary += (reg_to_num(arg1, line_no) << 7); // destination reg
+        binary += (reg_to_num(arg2, line_no) << 15); // source reg (rs1)
+        binary += (MASK11_0(validate_imm(arg3, 12, line_no)) << 20);
+        /* warn("Lab2-1 assignment: ANDI instruction\n");
+        exit(EXIT_FAILURE); */
     } else if (is_opcode(opcode) == LUI) {
         binary = (0x0D << 2) + 0x03;
         binary += (reg_to_num(arg1, line_no) << 7);
